22eeeada6ab38f682ba344c848718b6779cb414b
yuyaamano23 <nba.chako.nba@icloud.com>
2021-06-26 22:25:03 +0900

    カスタムフックの作成

--- hooks/authentication.ts
+++ hooks/authentication.ts
@@ -1,34 +1,41 @@
 import firebase from "firebase/app";
-import { User } from "../models/User";
+import { useEffect } from "react";
 import { atom, useRecoilState } from "recoil";
+import { User } from "../models/User";
 
 const userState = atom<User>({
 	key: "user",
 	default: null,
 });
 
-function useAuthentication() {
+export function useAuthentication() {
 	const [user, setUser] = useRecoilState(userState);
 
-	firebase
-		.auth()
-		.signInAnonymously()
-		.catch(function (error) {
-			// Handle Errors here.
-			console.error(error);
-		});
-
-	firebase.auth().onAuthStateChanged(function (firebaseUser) {
-		if (firebaseUser) {
-			setUser({
-				uid: firebaseUser.uid,
-				isAnonymous: firebaseUser.isAnonymous,
-			});
-		} else {
-			// User is signed out.
-			setUser(null);
+	useEffect(() => {
+		if (user !== null) {
+			return;
 		}
-	});
+
+		firebase
+			.auth()
+			.signInAnonymously()
+			.catch(function (error) {
+				// Handle Errors here.
+				console.error(error);
+			});
+
+		firebase.auth().onAuthStateChanged(function (firebaseUser) {
+			if (firebaseUser) {
+				setUser({
+					uid: firebaseUser.uid,
+					isAnonymous: firebaseUser.isAnonymous,
+				});
+			} else {
+				// User is signed out.
+				setUser(null);
+			}
+		});
+	}, []);
 
 	return { user };
 }
