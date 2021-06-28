0118941c449fa7e8b90de0e7d512d365a7941034
yuyaamano23 <nba.chako.nba@icloud.com>
2021-06-28 13:38:14 +0900

    dayjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import { RecoilRoot } from "recoil";
 import "../lib/firebase";
 import "../styles/globals.scss";
+import dayjs from "dayjs";
+import "dayjs/locale/ja";
+
+dayjs.locale("ja");
 
 function MyApp({ Component, pageProps }) {
 	return (
--- pages/questions/received.tsx
+++ pages/questions/received.tsx
@@ -3,6 +3,7 @@ import firebase from "firebase/app";
 import { Question } from "../../models/Question";
 import Layout from "../../components/Layout";
 import { useAuthentication } from "../../hooks/authentication";
+import dayjs from "dayjs";
 
 export default function QuestionsReceived() {
 	const [questions, setQuestions] = useState<Question[]>([]);
@@ -40,7 +41,25 @@ export default function QuestionsReceived() {
 
 	return (
 		<Layout>
-			<div>{questions.length}</div>
+			<h1 className="h4">受け取った質問一覧</h1>
+			<div className="row justify-content-center">
+				<div className="col-12 col-md-6">
+					{questions.map((question) => (
+						<div className="card my-3" key={question.id}>
+							<div className="card-body">
+								<div className="text-truncate">{question.body}</div>
+							</div>
+							<div className="text-muted text-end">
+								<small>
+									{dayjs(question.createdAt.toDate()).format(
+										"YYYY/MM/DD HH:mm"
+									)}
+								</small>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
 		</Layout>
 	);
 }
